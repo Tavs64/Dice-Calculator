fd59e0894a17dd2393e6b9143256edf785e4c2ca
Tavs64 <73593582+Tavs64@users.noreply.github.com>
2021-02-27 02:53:15 +0100

    Fixed the shit

--- main.py
+++ main.py
@@ -1,24 +1,75 @@
-import math                                                                                 #   The math library will allow us to make more complex equations
-import tkinter                                                                              #   The Tkinter library will allow us to make a GUI for our program
-import random                                                                               #   The Random library will allow us to generate random numbers
+import d20
+import tkinter
 
-def diceRoll(x,y):                                                                          #   Function that rolls the dice. x = Dice Count, y = Dice Type or Max Value, z = Minimum roll to succeed
-    global total                                                                            #   The total value will be used throughought different functions, and making it global will make it a smoother process
-    total = 0                                                                               #   Resets the total value before every roll
-    while (x != 0):                                                                         #   Loop that ensures that only the specified amount of dice are rolled
-        roll = random.randint(1,y)                                                          #   Generates a random number between 0 and the specified Max Value                                                                            #   The roll is added to the total
-        total = roll + total                                                                #   Adds the roll to the total
-        x = x-1                                                                             #   Decreases the dice count so that roll works correctly
+def Start():
+    print("Welcome to our Dice Calculator")
+    print("Are you using Dungeons & Dragons, or Warhammer 40.000")
+    global systemType
+    while True:
+        systemType = input("Select: ").lower().strip()
+        if (systemType == "dnd" or systemType == "d&d" or systemType == "dandd" or systemType == "dand" or systemType == "dungeons & dragons" or systemType == "dungeons and dragons"):
+            print("You Chose: Dungeons & Dragons")
+            dndStart()
+            break
+        elif (systemType == "warhammer" or systemType == "warhammer40k" or systemType == "wh40k" or systemType == "40k" or systemType == "warhammer40000" or systemType == "warhammer40.000"):
+            print("You Chose: Warhammer 40.000")
+            wh40kStart()
+            break
+        else:
+            print("Invalid Input")
+    
 
-def inputGet():                                                                             #   Function That Gets the Dice Info
-    global diceType                                                                         #   Declaring a variable that determines what type of die are we using
-    global diceCount                                                                        #   Declaring a variable that determines how many die are we using
-    diceType = int(input("What Type of Dice? "))                                            #   Takes which type of dice
-    diceCount = int(input("How Many Dice? "))                                               #   Takes how many dice 
+def dndStart():
+    print("What are you rolling for?")
+    global dndType
+    dndType = input("Attack, Skill Check, Saving Throw,") #this is unfineshed btw
 
-                                                                           
-inputGet()                                                                                  #   Gets the Inputs
-diceRoll(diceCount,diceType)                                                                #   Rolls the dice with the input gathered from inputGet
+def dndStats(): # This should only happen once, or when character is reset. perhaps we could save multiple characters. Nonetheless this should be replaced by the tkinter interface instead of a prompt. With the interface we could save different profiles
+    dndCharLvl = input("What is your Characters Level? ")
+    dndCharStr = input("What is your [Strength] Ability Score? ")
+    dndCharDex = input("What is your [Dexterity] Ability Score? ")
+    dndCharCon = input("What is your [Constitution] Ability Score? ")
+    dndCharInt = input("What is your [Intelligence] Ability Score? ")
+    dndCharWis = input("What is your [Wisdom] Ability Score? ")
+    dndCharCha = input("What is your [Charisma] Ability Score? ")
+    
+def dndDiceCalc():
+    global dndInput
+    while True:
+        dndInput = input("Dice Roll Input: ").lower().strip()
+        if (dndInput == "help"):
+            dndHelp()
+        elif (dndInput == "back"):
+            break
+        else:
+            try:
+                dndRoll = d20.roll(dndInput)
+                str(dndRoll)
+                print(" ")
+                print(dndRoll)
+                print(" ")
+            except:
+                print('Syntax Error: try writing "Help"')
 
-print(diceCount,'D',diceType,'Gave a Total of:',total)                                      #   Prints the Roll
+def dndDiceCharCalc():
+    print("Rolling Character Stats")
+    for n in range(6):
+        dndCharRoll = d20.roll("4d6kh3")
+        str(dndCharRoll)
+        print(dndCharRoll)
 
+    
+def dndHelp():
+    print("Inputs are formatted Dice-Count 'd' Dice-Type '+' Modifier")
+    print("Example: 1d20+2 ")
+    print('To Go back type "Back"')
+
+''' 
+def wh40kStart():
+
+def wh40kDiceCalc():
+
+def wh40kDiceInput():
+'''
+
+Start()
